a513f8a2bbe022d8e2be7d60363487178c619a8d
Hamza Ndiaye <hamzandiaye99@gmail.com>
2026-01-31 23:35:02 +0100

    Fix Vite base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/wondershop-client/", // 👈 NOM DU REPO GITHUB
+  base: "./",
+  // 👈 NOM DU REPO GITHUB
 
   plugins: [
     react(),
